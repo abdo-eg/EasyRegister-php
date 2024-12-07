0ab80c310c805fbb4892dea659a70da7f51a92b4
abdo-eg <alsharifegy@gmail.com>
2024-12-07 14:31:50 +0200

    add registration

--- register.php
+++ register.php
@@ -1,3 +1,44 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['register'])){
+
+	$name=mysqli_real_escape_string($conn, $_POST['name']);
+	$email=mysqli_real_escape_string($conn, $_POST['email']);
+	$password=mysqli_real_escape_string($conn, md5($_POST['password']));
+	$cpassword=mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+	$image=$_FILES['image']['name'];
+	$image_size=$_FILES['image']['size'];
+	$image_tmp_name=$_FILES['image']['tmp_name'];
+	$image_folder='uploaded_img/'.$image;
+	
+	$select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$password'") or die('query failed');
+	
+	if(mysqli_num_rows($select)>0){
+	    $message[]= 'User already exists';
+	}else{
+	    if($password !=$cpassword){
+	        $message[]= 'Two passwords are not matched!';
+	    }elseif($image_size > 2000000){
+	        $message[]='Image size is too large';
+	    }else{
+	    	$insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, image) VALUES('$name','$email','$password', '$image')") or die('query failed');
+	    	
+	    	if($insert){
+	    	    move_uploaded_file($image_tmp_name, $image_folder);
+	    	    $message[]='registered successfully!';
+	    	}else{
+	    	    $message[]='registeration failed!';
+	    	}
+	    }    
+	}
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -79,6 +120,16 @@
 <body>
     <div class="container">
         <h2>Register</h2>
+        <?php
+        if(isset($message)){
+        	foreach($message as $message){
+        	    echo'<div>'.$message.'</div>';
+        	}
+        }
+        
+        
+        
+        ?>
         <form action="register.php" method="POST" enctype="multipart/form-data">
             <label for="name">Name:</label>
             <input type="text" id="name" name="name" required>
@@ -95,7 +146,7 @@
             <label for="image">Upload Profile Image:</label>
             <input type="file" id="image" name="image" accept="image/*" required>
 
-            <button type="submit">Register</button>
+            <button type="submit" name="register">Register</button>
         </form>
 
         <p class="login-link">Already have an account? <a href="login.php">Login now</a></p>
