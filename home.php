10cd6dea786b1bb8603ee8f0a47c13e8592bb60c
abdo-eg <alsharifegy@gmail.com>
2024-12-07 17:44:37 +0200

    logout function

--- home.php
+++ home.php
@@ -5,6 +5,11 @@ $user_id = $_SESSION['user_id'];
 
 if(!isset($user_id)){
     header('location:login.php');
+};
+if(isset($_GET['logout'])){
+    unset($user_id);
+    session_destroy();
+    header('location:login.php');
 }
 ?>
 
